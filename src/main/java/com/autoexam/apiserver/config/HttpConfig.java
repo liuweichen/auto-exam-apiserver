0b0bd6e0dae24c136b427bc0c31aa17d6d2c0c01
liuweichen <754075714@qq.com>
2020-10-01 22:22:05 +0800

    增加对https的支持

---
+++ src/main/java/com/autoexam/apiserver/config/HttpConfig.java
@@ -0,0 +1,27 @@
+package com.autoexam.apiserver.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpConfig {
+  @Value("${server.http.port:8080}")
+  private int httpPort;
+
+  @Bean
+  public ServletWebServerFactory servletContainer() {
+    TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+    tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+    return tomcat;
+  }
+
+  private Connector createStandardConnector() {
+    Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+    connector.setPort(httpPort);
+    return connector;
+  }
+}
