6459662e7bf4ade25416dc16fc50581e1a2ec7f6
liuweichen <754075714@qq.com>
2020-10-03 20:53:43 +0800

    配置文件中明文密码加密

--- src/test/java/com/autoexam/apiserver/ApiServerApplicationTests.java
+++ src/test/java/com/autoexam/apiserver/ApiServerApplicationTests.java
@@ -1,16 +1,28 @@
 package com.autoexam.apiserver;
 
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApiServerApplicationTests {
+	@Autowired
+	StringEncryptor encryptor;
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testEncrypt() {
+		String encrypt = encryptor.encrypt("test");
+		String origin = encryptor.decrypt(encrypt);
+		Assert.assertEquals("test", origin);
 	}
 
 }
